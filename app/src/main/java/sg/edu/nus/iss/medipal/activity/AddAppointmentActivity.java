0258be3ddbc7a62c9e82ad0cc213a328a4995865
Navi <naval.kumar99@gmail.com>
2017-03-15 01:40:49 +0800

    saving work for add appointment

--- app/src/main/java/sg/edu/nus/iss/medipal/activity/AddAppointmentActivity.java
+++ app/src/main/java/sg/edu/nus/iss/medipal/activity/AddAppointmentActivity.java
@@ -2,6 +2,7 @@ package sg.edu.nus.iss.medipal.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import sg.edu.nus.iss.medipal.R;
 
@@ -11,6 +12,11 @@ public class AddAppointmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appointment_add);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.tb_addapp);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
 }
