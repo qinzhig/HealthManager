5152b11e479291ae8438135540ab58f78116b7cc
Naval Kumar Nanjappa <e0146820@nus.edu.sg>
2017-03-14 20:59:13 +0800

    saving work done for adding appointment

---
+++ app/src/main/java/sg/edu/nus/iss/medipal/activity/AddAppointmentActivity.java
@@ -0,0 +1,16 @@
+package sg.edu.nus.iss.medipal.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sg.edu.nus.iss.medipal.R;
+
+public class AddAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appointment_add);
+
+    }
+}
--- app/src/main/java/sg/edu/nus/iss/medipal/fragment/AppointmentFragment.java
+++ app/src/main/java/sg/edu/nus/iss/medipal/fragment/AppointmentFragment.java
@@ -1,7 +1,9 @@
 package sg.edu.nus.iss.medipal.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,11 +12,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import sg.edu.nus.iss.medipal.R;
+import sg.edu.nus.iss.medipal.activity.AddAppointmentActivity;
 import sg.edu.nus.iss.medipal.adapter.AppointmentAdapter;
 import sg.edu.nus.iss.medipal.dao.AppointmentDAO;
 import sg.edu.nus.iss.medipal.pojo.Appointment;
@@ -43,26 +47,45 @@ public class AppointmentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        View appointmentFragment;
+        FloatingActionButton aFab;
+
         //using the appointment_main xml to show in the main fragment
-        View appointmentFragment = inflater.inflate(R.layout.appointment_main,container,false);
+        appointmentFragment = inflater.inflate(R.layout.appointment_main, container, false);
 
         //get reference to the current context(
         mContext = appointmentFragment.getContext();
-
-        //get reference to the recyclerview
-        appointmentsView = (RecyclerView) appointmentFragment.findViewById(R.id.appointmentrecycler_view);
-        //get the reference to appointmentDAO opbject
+        //get the reference to appointmentDAO object
         appointmentDAO = new AppointmentDAO(mContext);
-
         //get the appointment details from the appointment table
-        //appointmentList = appointmentDAO.getAppointments();
+        appointmentList = appointmentDAO.getAppointments();
 
         appointmentList = new ArrayList<Appointment>();
         appointmentList.add(new Appointment(1,"Singapore","01-04-2017","Test Appointment 1"));
         appointmentList.add(new Appointment(2,"Singapore","02-04-2017","Test Appointment 2"));
 
 
-        populateRecyclerView();
+        if(appointmentList.isEmpty())
+        {
+            appointmentFragment = inflater.inflate(R.layout.message_placeholder,container,false);
+            TextView txtView = (TextView) appointmentFragment.findViewById(R.id.placeholdertext);
+            txtView.setText("No appointments found");
+        }
+        else {
+            //get reference to the recyclerview
+            appointmentsView = (RecyclerView) appointmentFragment.findViewById(R.id.appointmentrecycler_view);
+
+            populateRecyclerView();
+
+        }
+
+        aFab = (FloatingActionButton)appointmentFragment.findViewById(R.id.fab);
+        aFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(mContext, AddAppointmentActivity.class));
+            }
+        });
 
         return appointmentFragment;
     }
