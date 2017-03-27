79c0a08bd4e0176cd6760035c056231bec9e7261
DESKTOP-FV3AR0O\levis <leviskim17@gmail.com>
2017-03-27 18:47:57 +0900

    add AboutActivity.

---
+++ app/src/main/java/sg/edu/nus/iss/medipal/activity/AboutActivity.java
@@ -0,0 +1,67 @@
+package sg.edu.nus.iss.medipal.activity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import sg.edu.nus.iss.medipal.R;
+
+/**
+ * Created by levis on 3/27/2017.
+ */
+
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener  {
+    private TextView _contactAddressView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.about_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
+        _contactAddressView = (TextView) findViewById(R.id.about_contactaddress);
+        _contactAddressView.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_action_close, menu);
+
+        final MenuItem menuItem = menu.findItem(R.id.action_close);
+        menuItem.getActionView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == _contactAddressView) {
+
+            String contactAddress = _contactAddressView.getText().toString();
+            String subject = "";
+            String body  = "";
+
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse(contactAddress));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+
+            emailIntent.setType("message/rfc822");
+
+            startActivity(Intent.createChooser(emailIntent, "Choose an Email client :"));
+        }
+    }
+}
